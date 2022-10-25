04acb8fc454942e8e26c3cf6634e2f1dd3fb1876
chan1122 <zoro3958@naver.com>
2022-10-25 16:18:03 +0900

    aa

---
+++ 01.JAVA-Root/javadefault/src/JAVA01.java
@@ -0,0 +1,38 @@
+public class JAVA01 {
+    public static void main(String[] args) {
+        // 자바 클래스는 소스 단위블록
+        // 자바 main매서드는 클래스의 첫실행 매서드
+        // 자바한줄 주석
+        /* 여러줄 주석 */
+
+        // 자바 출력 1  : println() - 
+        // 뒤에 엔터기호가 들어가 있는 출력 메서드
+        // 단축키 : syso 
+        System.out.println("자바 구문을 알아보장~!");
+
+        // 자바 출력 2  : print()  
+        // - 뒤에 엔터 기호가 없어서 한줄에 나옴!!
+        System.out.print("자");
+        System.out.print("바");
+        System.out.print("다!");
+        System.out.println("너두냐?????");
+
+        // 홑따옴표
+        System.out.println('홑');
+        // 홑따옴표 -> 한글자만 출력됨
+        // System.out.println('홑따옴표'); -> 에러!!
+
+        // 숫자출력
+        System.out.println(3);
+        System.out.println(305);
+        System.out.println(3050000);
+
+        // 숫자 계산
+        System.out.println(3*7);
+        System.out.println(1+2+3+4+5+6);
+        System.out.println(210/10);
+
+
+
+    }
+}
