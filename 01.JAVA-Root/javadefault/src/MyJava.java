0017514cda43cb4bb07804d869719faac7b4dd77
chan1122 <zoro3958@naver.com>
2022-10-25 15:57:51 +0900

    aa

--- 01.JAVA-Root/javadefault/src/MyJava.java
+++ 01.JAVA-Root/javadefault/src/MyJava.java
@@ -1,6 +1,6 @@
 public class MyJava{
     public static void main(String[] args) {
-        System.out.println("나야나!~!");
-        System.out.println("나아니냐!~!");
+        System.out.println("로딩 1111111111111");
+        System.out.println("로딩 확인 22222222");
     }
 }
