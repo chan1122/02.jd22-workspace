118000a21af6351d17ca71351789c47f94cb0158
chan1122 <zoro3958@naver.com>
2022-10-31 15:39:46 +0900

    aa

--- 01.JAVA-Root/javadefault/src/JAVA01.java
+++ 01.JAVA-Root/javadefault/src/JAVA01.java
@@ -1,76 +1,57 @@
 public class JAVA01 {
     public static void main(String[] args) {
         // 자바 클래스는 소스 단위블록
-        // 자바 main매서드는 클래스의 첫실행 매서드
-        // 자바한줄 주석
-        /* 여러줄 주석 */
+        // 자바 main메서드는 클래스의 첫실행 메서드
+        // 자바한줄주석
+        /* 여러줄주석 */
 
-        // 자바 출력 1  : println() - 
-        // 뒤에 엔터기호가 들어가 있는 출력 메서드
+        // 자바출력1 : println() 
+        // - 뒤에 엔터기호가 들어가 있는 출력메서드
         // 단축키 : syso 
-        System.out.println("자바 구문을 알아보장~!");
-
-        // 자바 출력 2  : print()  
-        // - 뒤에 엔터 기호가 없어서 한줄에 나옴!!
-        System.out.print("자");
-        System.out.print("바");
-        System.out.print("다!");
-        System.out.println("너두냐?????");
-
-        // 홑따옴표
-        System.out.println('홑');
-        // 홑따옴표 -> 한글자만 출력됨
-        // System.out.println('홑따옴표'); -> 에러!!
+        System.out.println("자바구문을 알자!");
+        // 자바출력2 : print()
+        // - 뒤에 엔터기호가 없어서 한줄에 나옴!
+        System.out.print("나");
+        System.out.print("야");
+        System.out.print("나!");
+        System.out.println("너두냐?");
+        // 홑따옴표 -> 한글자만 출력됨!
+        System.out.println('흥');
+        // System.out.println('홑따옴표');
 
         // 숫자출력
         System.out.println(3);
-        System.out.println(305);
-        System.out.println(3050000);
+        System.out.println(365);
+        System.out.println(10000);
 
-        // 숫자 계산
-        System.out.println(3*7);
-        System.out.println(1+2+3+4+5+6);
-        System.out.println(210/10);
-
-/*         [ 자바구문 ]
+        // 숫자계산
+        System.out.println(3+3);
+        System.out.println(3*3);
+        System.out.println(500/5);
+        
+       /*  [ 자바구문 ]
 
         Java에서 실행되는 모든 코드 라인은 class
         이 예에서는 클래스 이름을 Main 으로 지정
         클래스는 항상 대문자로 시작
-    
-    참고: Java는 대소문자를 구분합니다. 
-    "MyClass"와 "myclass"는 의미가 다름
-    
-    Java 파일의 이름 은 클래스 이름 과 일치해야함!!! 
-    파일을 저장할 때 클래스 이름으로 저장
-    파일 이름 끝에 확장자로 ".java"를 사용함
-    
-    public static void main(String[] args){}
-    클래스에는 반드시 main()가 있어야함. 그래야 실행됨.
-    
-    한 줄 텍스트를 화면에 출력
-    System.out.println() -> 뒤에 줄바꿈 있음!
-    System.out.print() -> 뒤에 줄바꿈 없음
-    
-    System.out - Java 내장클래스
-    println() "print line"의 줄임말
-    
-    참고: 중괄호 {}는 코드 블록의 시작과 끝을 표시 */
 
+        참고: Java는 대소문자를 구분합니다. 
+        "MyClass"와 "myclass"는 의미가 다름
 
+        Java 파일의 이름 은 클래스 이름 과 일치해야함!!! 
+        파일을 저장할 때 클래스 이름으로 저장
+        파일 이름 끝에 확장자로 ".java"를 사용함
 
+        public static void main(String[] args){}
+        클래스에는 반드시 main()가 있어야함. 그래야 실행됨.
 
+        한 줄 텍스트를 화면에 출력
+        System.out.println() -> 뒤에 줄바꿈 있음!
+        System.out.print() -> 뒤에 줄바꿈 없음
 
+        System.out - Java 내장클래스
+        println() "print line"의 줄임말
 
-
-
-
-
-
-
-
-
-
-
+        참고: 중괄호 {}는 코드 블록의 시작과 끝을 표시 */
     }
 }
---
+++ 01.JAVA-Root/javadefault/src/JAVA05.java
@@ -0,0 +1,8 @@
+public class JAVA05 {
+    public static void main(String[] args) {
+        myFn();
+    }
+    static void myFn(){
+        System.out.println(333);
+    }
+}
